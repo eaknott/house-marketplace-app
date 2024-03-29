eb5f93372fcdf2cc83f22909a1516a710e330c4e
eaknott <emknotty@gmail.com>
2024-03-29 17:17:55 +0800

    Add user registration functionality to SignUp component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -21,6 +23,25 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
     return (
       <>
         <div className="pageContainer">
@@ -29,7 +50,7 @@ function SignUp() {
           </header>
 
           <main>
-            <form>
+            <form onSubmit={onSubmit}>
             <input 
                 type="text" 
                 placeholder="Name" 

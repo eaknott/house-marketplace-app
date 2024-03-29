0981bae8e840d7fe7476f78a217009e974dbfe80
eaknott <emknotty@gmail.com>
2024-03-29 16:58:18 +0800

    Add routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Explore from './pages/Explore'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   return (
     <>
-      <h2>My App</h2>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
+
   );
 }
 

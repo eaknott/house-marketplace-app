0c3c65b5ece288ca8d287eff8c45d8511c953e8c
eaknott <emknotty@gmail.com>
2024-03-30 14:34:04 +0800

    Set up new user sign-up functionality and link to firestore database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRig
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { db } from '../firebase.config'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -29,12 +30,18 @@ function SignUp() {
       const auth = getAuth()
 
       const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-      
+       
       const user = userCredential.user
 
       updateProfile(auth.currentUser, {
         displayName: name
       })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
 
       navigate('/')
     } catch (error) {

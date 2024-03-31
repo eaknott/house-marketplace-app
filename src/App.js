541ad164654801fb2c1ee24398fac9dd8474d15a
eaknott <emknotty@gmail.com>
2024-03-31 13:01:04 +0800

    Add categories with images and Link to category pages through Explore page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Profile from './pages/Profile'
 import Offers from './pages/Offers'
+import Category from './pages/Category';
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>

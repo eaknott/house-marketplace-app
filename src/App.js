e6f59bf3d723c0f6c6aa6251aef334e0a8758ce3
eaknott <emknotty@gmail.com>
2024-03-31 12:17:44 +0800

    Add PrivateRoute component to use firebase/auth and keep user logged in, even when refreshing the page/window

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Profile from './pages/Profile'
@@ -16,7 +17,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />

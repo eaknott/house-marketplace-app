f060604eab20b4d5cad66a20dada271f5f05bde4
eaknott <emknotty@gmail.com>
2024-04-01 11:29:20 +0800

    Add Listing component for individual listings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import PrivateRoute from './components/PrivateRoute';
+import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from './components/PrivateRoute'
 import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Profile from './pages/Profile'
 import Offers from './pages/Offers'
-import Category from './pages/Category';
+import Category from './pages/Category'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
-import CreateListing from './pages/CreateListing';
+import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
+
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>
